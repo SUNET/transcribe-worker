8bad635c801be87b024817c515cd36721bb8abee
Kristofer Hallin <kristofer@sunet.se>
2025-06-12 09:17:07 +0200

    Bug: Hour was missing from subtitle timestamp.

--- utils/whisper.py
+++ utils/whisper.py
@@ -195,10 +195,11 @@ class WhisperAudioTranscriber:
         """
         Format a timestamp in seconds to MM:SS format.
         """
+        hours = int(seconds // 3600)
         minutes = int(seconds // 60)
         seconds = int(seconds % 60)
 
-        return f"{minutes:02d}:{seconds:02d}"
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
     def __caption_split(self, caption):
         """
