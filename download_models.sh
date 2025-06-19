35f449ba551345425a5aebee665dea6ddf520b4f
Kristofer Hallin <kristofer@sunet.se>
2025-06-19 11:09:28 +0200

    Set correct model names in the download_models script.

--- download_models.sh
+++ download_models.sh
@@ -9,8 +9,10 @@ for i in tiny base small medium large; do
 done
 
 # Download other models
-for i in tiny base small medium; do
-	wget -q --show-progress -O models/whisper_${i}.bin https://huggingface.co/ggerganov/whisper.cpp/blob/main/ggml-${i}-q5_1.bin
+for i in tiny base small; do
+	wget -q --show-progress -O models/whisper_${i}.bin https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-${i}-q5_1.bin
 done
 
-wget -q --show-progress -O models/whisper_large.bin https://huggingface.co/ggerganov/whisper.cpp/blob/main/ggml-large-v2-q5_0.bin
+wget -q --show-progress -O models/whisper_medium.bin https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-medium-q5_0.bin
+
+wget -q --show-progress -O models/whisper_large.bin https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-large-v2-q5_0.bin
