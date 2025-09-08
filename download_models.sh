360e6554377e873b503f60c00a176a49ff8eefe5
Kristofer Hallin <kristofer@sunet.se>
2025-09-08 09:27:44 +0200

    Add Norwegian models to download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -3,9 +3,10 @@
 # Create models/ if needed
 mkdir -p models
 
-# Download the KB models
+# Download Swedish and Norwegian models
 for i in tiny base small medium large; do
 	wget -q --show-progress -O models/sv_${i}.bin https://huggingface.co/KBLab/kb-whisper-${i}/resolve/main/ggml-model-q5_0.bin
+	wget -q --show-progress -O models/no_${i}.bin https://huggingface.co/NbAiLab/nb-whisper-${i}/resolve/main/ggml-model-q5_0.bin
 done
 
 # Download other models
@@ -14,5 +15,4 @@ for i in tiny base small; do
 done
 
 wget -q --show-progress -O models/whisper_medium.bin https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-medium-q5_0.bin
-
 wget -q --show-progress -O models/whisper_large.bin https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-large-v2-q5_0.bin
