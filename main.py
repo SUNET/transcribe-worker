0053290146d3dd69594c5af5f44b399d4b3f6ccc
Kristofer Hallin <kristofer@sunet.se>
2025-10-17 13:04:55 +0200

    Add healthcheck.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import gpustat
 import os
+import psutil
+import requests
 import sys
 import threading
 
@@ -16,6 +19,50 @@ foreground, pidfile, zap, _, _, _ = parse_arguments()
 
 if not zap:
     from utils.job import TranscriptionJob
+
+
+def healthcheck() -> None:
+    while True:
+        # Gather load average, memory usage and GPU usage
+        load_avg = psutil.cpu_percent()
+        memory_usage = psutil.virtual_memory().percent
+        gpu_usage = []
+
+        try:
+            gpu_stat = gpustat.GPUStatCollection.new_query()
+        except Exception:
+            gpu_stat = []
+
+        for gpu in gpu_stat:
+            gpu_usage.append(
+                {
+                    "index": gpu.index,
+                    "name": gpu.name,
+                    "memory_used": gpu.memory_used,
+                    "memory_total": gpu.memory_total,
+                    "utilization": gpu.utilization,
+                }
+            )
+
+        health_data = {
+            "worker_id": os.uname()[1],
+            "load_avg": load_avg,
+            "memory_usage": memory_usage,
+            "gpu_usage": gpu_usage,
+        }
+
+        try:
+            res = requests.post(
+                f"{settings.API_BACKEND_URL}/api/{settings.API_VERSION}/healthcheck",
+                json=health_data,
+                cert=(settings.SSL_CERTFILE, settings.SSL_KEYFILE),
+            )
+            res.raise_for_status()
+        except requests.RequestException as e:
+            logger.error(f"Healthcheck failed: {e}")
+
+        logger.debug("Healthcheck: Sent")
+        sleep(10)
 
 
 def mainloop(worker_id: int) -> None:
@@ -44,6 +91,10 @@ def mainloop(worker_id: int) -> None:
 
 def main() -> None:
     logger.info("Starting transcription service...")
+
+    # Start the healthcheck thread
+    health_thread = threading.Thread(target=healthcheck)
+    health_thread.start()
 
     for i in range(settings.WORKERS):
         thread = threading.Thread(target=mainloop, args=(i,))
